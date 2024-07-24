3a6cab5c61e28a6a2c4b9751bfdd52d30e502cf7
Hachiko <deniszhukov.hachiko@gmail.com>
2024-07-24 10:20:35 +0300

    refactor

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-
-const app = express()
-
-app.use(serveStatic('public', { index: ['index.html', 'index.htm'] }))
-app.listen(3000)
